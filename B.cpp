81654fa3dc4a45d47860147effbd9551813d39f4
Caquizeraa <isacgcunha@gmail.com>
2024-10-20 12:40:54 -0300

    Adiciona A e B

---
+++ B.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+class B{
+private:
+    int B1;
+    float B2;
+public:
+    int getB1(){
+        return B1;
+    }
+    
+    float getB2(){
+        return B2;
+    }
+
+    void setB1(int B1){
+        this->B1 = B1;
+    }
+
+    void setB2(float B2){
+        this->B2 = B2;
+    }
+
+    void MB1(){
+        cout<<"MB1";
+    }
+
+    void MB2(){
+        cout<<"MB2";
+    }
+};
